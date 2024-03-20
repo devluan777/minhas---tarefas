3a02e58e5613ec8ec1575b0f20e41eb3124e3615
schorchi <152038335+schorchi-GH@users.noreply.github.com>
2024-03-20 07:04:52 -0300

    Marcando uma tarefa como concluída (32.9)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,4 @@
 import styled, { createGlobalStyle } from 'styled-components'
-import { Botao } from '../components/Tarefa/styles'
 import variaveis from './variaveis'
 
 const EstiloGlobal = createGlobalStyle`
@@ -11,12 +10,10 @@ const EstiloGlobal = createGlobalStyle`
     list-style: none;
   }
 `
-
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 224px auto;
 `
-
 export const MainContainer = styled.main`
   padding: 0 40px;
   height: 100vh;
@@ -38,6 +35,18 @@ export const Campo = styled.input`
   border-color: #666666;
   width: 100%;
 `
+export const Botao = styled.button`
+  font-weight: bold;
+  font-size: 12px;
+  color: #fff;
+  padding: 8px 12px;
+  border: none;
+  cursor: pointer;
+  background-color: ${variaveis.azulEscuro};
+  border-radius: 8px;
+  margin-right: 8px;
+`
+
 export const BotaoSalvar = styled(Botao)`
   background-color: ${variaveis.verde};
 `
