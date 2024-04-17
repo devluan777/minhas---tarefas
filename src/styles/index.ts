64c28994152f27e48ca4f89d693255bbe655fb65
devluan777 <zcanaldedois@gmail.com>
2024-04-16 22:50:59 -0300

    Adicionando a primeira parte do responsivo

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,19 +12,31 @@ const EstiloGlobal = createGlobalStyle`
 `
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 224px auto;
+  grid-template-columns: auto;
+  @media (min-width: 768px) {
+    grid-template-columns: 224px auto;
+  }
 `
 export const MainContainer = styled.main`
-  padding: 0 40px;
-  height: 100vh;
+  padding: 0 20px;
   overflow-y: scroll;
+  @media (min-width: 768px) {
+    padding: 0 40px;
+    height: 100vh;
+  }
 `
+
 export const Titulo = styled.h2`
   display: block;
-  margin-top: 40px;
-  margin-bottom: 40px;
-  font-size: 18px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 16px;
   font-weight: bold;
+  @media (min-width: 768px) {
+    margin-top: 40px;
+    margin-bottom: 40px;
+    font-size: 18px;
+  }
 `
 export const Campo = styled.input`
   padding: 8px;
